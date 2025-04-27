cf3efc9d42124728117b8bd3c4f4f5b1d0fd167e
Zevi Liao <jointjm@gmail.com>
2025-04-27 19:05:51 +0800

    setState by function

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,20 +2,22 @@
 import React, { useState } from "react";
 
 const Counter = () => {
-  // let count = 0;
-  const [count, setCount] = useState(0)
-  
-  console.log('[count-out]',count)
+  const [count, setCount] = useState(0);
+
+  console.log("[count-out]", count);
 
   return (
     <>
       <h3>Counter = {count}</h3>
       <button
         onClick={() => {
-          // count += 1
-          setCount(count+ 1)
-          console.log('[count]','clicked')
-          console.log('[count-in]',count) // got old value.
+          // setCount(count+ 1)
+          setCount((prev) => {
+            const state = prev + 1;
+            console.log("[count-in]", state);
+            return state;
+          });
+          console.log("[count]", "clicked");
         }}
       >
         add count
