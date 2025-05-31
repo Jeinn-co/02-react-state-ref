353e57997817cc6385e2d97dc7679e708e0b3e55
Zevi Liao <jointjm@gmail.com>
2025-06-01 01:24:35 +0800

    no re-render value

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-// import MyButton from "./components/MyButton";
+import MyButton from "./components/MyButton";
 // import MyTitle from "./components/MyTitle";
-import Counter from "./components/Counter";
+// import Counter from "./components/Counter";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,9 +14,9 @@ function App() {
       <button> click me! </button> */}
       {/* <MyTitle userName="John">
         <div style={{ color: "lightgreen" }}>children content</div>
-      </MyTitle>
-      <MyButton></MyButton> */}
-      <Counter/>
+      </MyTitle>*/}
+      <MyButton></MyButton> 
+      {/* <Counter/> */}
     </>
   );
 }
--- src/components/MyButton.jsx
+++ src/components/MyButton.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { useRef, useEffect } from "react";
 
 const MyButton = () => {
-  return (
-    <button> click me! </button> 
-  )
-}
+  const count = useRef(0);
 
-export default MyButton
+  const handleClick = () => {
+    count.current += 1;
+    console.log('[count]', count.current)
+    console.log("clicked");
+  };
+
+  useEffect(() => {
+    console.log('[effect run]')
+  })
+
+  return (
+    <>
+      <h2>count: {count.current}</h2>
+      <button onClick={handleClick}> click me! </button>
+    </>
+  );
+};
+
+export default MyButton;
